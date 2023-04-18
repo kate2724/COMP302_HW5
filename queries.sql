71fd8701cac1ce0fbf355f6650faf7e6366c2a6e
Katelyn Breen <60229563+kate2724@users.noreply.github.com>
2023-04-18 12:42:30 -0500

    update

---
+++ queries.sql
@@ -0,0 +1,7 @@
+--query 1: Return all the pages that link to the page “Apple”
+
+
+--query 2: Return the page that has been revised the most
+
+
+--query 3: Return all the editors of a particular page
--- wikipedia_database.sql
+++ wikipedia_database.sql
@@ -1,45 +1,63 @@
-CREATE TABLE page(
-    page_id INT NOT NULL PRIMARY KEY,
-    page_name VARCHAR(32),
-    restriction_level VARCHAR(32),
-    page_length INT,
-)engine=innodb;
+SET FOREIGN_KEY_CHECKS=0;
 
-CREATE TABLE Link(
-    link_id INT NOT NULL PRIMARY KEY,
-    from_url VARCHAR(32),
-    to_url VARCHAR(32),
+-- ------------------   page -- -------------------------------
+DROP TABLE IF EXISTS jfj;
+
+CREATE TABLE page(
+    page_id             INT         NOT NULL PRIMARY KEY,
+    page_name           VARCHAR(32),
+    restriction_level   VARCHAR(32),
+    page_length         INT,
+);
+
+-- ------------------   link -- -------------------------------
+DROP TABLE IF EXISTS link;
+
+CREATE TABLE link(
+    link_id     INT         NOT NULL PRIMARY KEY,
+    from_url    VARCHAR(32),
+    to_url      VARCHAR(32),
     is_external BOOLEAN,
-    links_from INT,
-    links_to INT,
+    links_from  INT,
+    links_to    INT,
+
     FOREIGN KEY (links_from) REFERENCES page(page_id),
     FOREIGN KEY (links_to) REFERENCES page(page_id)
-)engine=innodb;
+);
 
-CREATE TABLE User(
-    user_id INT NOT NULL PRIMARY KEY,
-    user_name VARCHAR(32),
-    user_password VARCHAR(32),
-    user_email VARCHAR(32),
-    user_group VARCHAR(32),
-    last_login DATE,
-    ip_address INT,
+-- ------------------   user -- -------------------------------
+DROP TABLE IF EXISTS user;
+
+CREATE TABLE user(
+    user_id         INT             NOT NULL PRIMARY KEY,
+    user_name       VARCHAR(32),
+    user_password   VARCHAR(32),
+    user_email      VARCHAR(32),
+    user_group      VARCHAR(32),
+    last_login      DATE,
+    ip_address      INT,
     registration_date DATE
-)engine=innodb;
+);
+
+-- ------------------   revision -- -------------------------------
+DROP TABLE IF EXISTS revision;
 
 CREATE TABLE revision(
     revision_id INT NOT NULL PRIMARY KEY,
     date_time DATE,
     rev_length INT,
     page_id INT,
+    user_id INT,
     FOREIGN KEY (user_id) REFERENCES user(user_id),
     FOREIGN KEY (page_id) REFERENCES page(page_id)
-)engine=innodb;
+);
 
-CREATE TABLE revision_comment(
-    revision_id INT,
-    comment_hash INT,
-    comment_test VARCHAR(32),
-    FOREIGN KEY (revision_id) REFERENCES revision(revision_id),
-    PRIMARY KEY (revision_id)
-)
+-- CREATE TABLE revision_comment(
+--     revision_id INT,
+--     comment_hash INT,
+--     comment_test VARCHAR(32),
+--     FOREIGN KEY (revision_id) REFERENCES revision(revision_id),
+--     PRIMARY KEY (revision_id)
+-- )
+
+SET FOREIGN_KEY_CHECKS=1;
