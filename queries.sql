2dcdc2dc455f9aca811e90e5f4c5142f41ccbead
Katelyn Breen <60229563+kate2724@users.noreply.github.com>
2023-04-18 22:51:40 -0500

    added revision data

--- queries.sql
+++ queries.sql
@@ -1,26 +1,26 @@
 --query 1: Return all the pages that the page "Apple" links to
-SHOW TABLES;
 
-DROP TABLE IF EXISTS q1;
-CREATE TABLE q1 AS
-SELECT DISTINCT to_id
-FROM
-    (SELECT * 
-    FROM w_page
-    WHERE page_name = 'Apple') A
-    JOIN
-    link B
-    ON A.page_id = B.from_id;
-ALTER TABLE q1 ADD PRIMARY KEY(page_id);
-SELECT * FROM q1 LIMIT 5;
+-- DROP TABLE IF EXISTS q1;
+-- CREATE TABLE q1 AS
+-- SELECT DISTINCT to_id
+-- FROM
+--     (SELECT * 
+--     FROM w_page
+--     WHERE page_name = 'Apple') A
+--     JOIN
+--     link B
+--     ON A.page_id = B.from_id;
+-- ALTER TABLE q1 ADD PRIMARY KEY(page_id);
+
+-- SELECT * FROM q1 LIMIT 5;
 
 --query 2: Return the page that has been revised the most
 
 DROP TABLE IF EXISTS revision_count_by_page;
-DROP TABLE IF EXISTS q2;
+DROP TABLE IF EXISTS max_revision_count_with_page;
 
 CREATE TABLE revision_count_by_page AS
-SELECT page_id, COUNT(revision_id) AS revision_count
+SELECT page_id, COUNT(r_date) AS revision_count
 FROM revision
 GROUP BY page_id;
 
@@ -30,7 +30,7 @@ FROM revision_count_by_page
 GROUP BY page_id;
 
 
-SELECT * FROM q2;
+SELECT * FROM revision_count_by_page;
 
 --query 3: Return all the editors of a particular page
 
