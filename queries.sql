fc83b816fe7183d0bc8ed3251681fbb426903035
Katelyn Breen <60229563+kate2724@users.noreply.github.com>
2023-04-18 23:12:18 -0500

    edited query 3 to select distinct

--- queries.sql
+++ queries.sql
@@ -1,53 +1,48 @@
 --query 1: Return all the pages that the page "Apple" links to
 
+
+
 -- DROP TABLE IF EXISTS q1;
 -- CREATE TABLE q1 AS
 -- SELECT DISTINCT to_id
 -- FROM
+--     link A
+--     JOIN
 --     (SELECT * 
 --     FROM w_page
---     WHERE page_name = 'Apple') A
---     JOIN
---     link B
---     ON A.page_id = B.from_id;
+--     WHERE page_name = 'Apple') B
+--     ON A.from_id = B.page_id;
 -- ALTER TABLE q1 ADD PRIMARY KEY(page_id);
-
-DROP TABLE IF EXISTS q1;
-CREATE TABLE q1 AS
-SELECT DISTINCT to_id
-FROM
-    link A
-    JOIN
-    (SELECT * 
-    FROM w_page
-    WHERE page_name = 'Apple') B
-    ON A.from_id = B.page_id;
-ALTER TABLE q1 ADD PRIMARY KEY(page_id);
-SELECT * FROM q1 LIMIT 5;
+-- SELECT * FROM q1 LIMIT 5;
 
 --query 2: Return the page that has been revised the most
 
-DROP TABLE IF EXISTS revision_count_by_page;
-DROP TABLE IF EXISTS max_revision_count_with_page;
+-- DROP TABLE IF EXISTS revision_count_by_page;
+-- DROP TABLE IF EXISTS max_revision_count_with_page;
 
-CREATE TABLE revision_count_by_page AS
-SELECT page_id, COUNT(r_date) AS revision_count
-FROM revision
-GROUP BY page_id;
+-- CREATE TABLE revision_count_by_page AS
+-- SELECT page_id, COUNT(r_date) AS revision_count
+-- FROM revision
+-- GROUP BY page_id;
 
-CREATE TABLE max_revision_count_with_page AS
-SELECT page_id, MAX(revision_count) AS max_revisions
-FROM revision_count_by_page;
-GROUP BY page_id;
+-- CREATE TABLE max_revision_count_with_page AS
+-- SELECT page_id
+-- FROM revision_count_by_page
+-- WHERE max_revisions = ( SELECT MAX(revision_count) FROM revision_count_by_page) ;
+-- -- GROUP BY page_id;
 
 
-SELECT * FROM max_revision_count_with_page;
+-- SELECT * FROM max_revision_count_with_page;
 
 --query 3: Return all the editors of a particular page
 
 DROP TABLE IF EXISTS q3;
 
 CREATE TABLE q3 AS
-SELECT user_id FROM revision WHERE page_id = 18978754;
+SELECT DISTINCT user_id 
+FROM revision 
+WHERE page_id = 18978754;
+
+SELECT * FROM q3;
 
 DROP TABLE IF EXISTS q3;
